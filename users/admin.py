dc1aa179e9e1437f0154ce30e2b7d1024300082b
Nikhil Patil <nikhilpatil3701@gmail.com>
2025-10-14 00:13:33 +0530

    dev: Modify the User model and register it on the admin site.

--- users/admin.py
+++ users/admin.py
@@ -4,7 +4,7 @@ from .models import User
 @admin.register(User)
 class UserAdmin(admin.ModelAdmin):
     list_display = ('id', 'first_name',"last_name", "username", "phone_number", "is_superuser", "created_at", "updated_at")
-    search_fields = ('id', 'first_name',"last_name", "username", "phone_number", "is_superuser", "created_at", "updated_at")
+    list_display_links  = ('id', 'first_name',"last_name", "username", "phone_number", "is_superuser", "created_at", "updated_at")
     list_filter = ('is_active',)
     ordering = ('-id',)
     list_per_page = 10
